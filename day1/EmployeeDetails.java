5fc352e4da72f582835670641db4c42a6dd50129
Vijayalakshmi Gunasekaran <vijiguna1995@gmail.com>
2025-12-06 20:29:53 +0530

    Add files via upload
    Day1

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		
+		String name = "John Doe";
+		byte age = 30;
+		float salary = 55000.75f;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210L;
+	    int pinCode = 56001;
+		byte luckyNumber = 7;
+		System.out.println(name+"\n"+age+"\n"+ salary+"\n"+gender+"\n"+acceptTerms+"\n"+rating+"\n"+ mobileNumber +"\n"+pinCode+"\n"+ luckyNumber);
+		
+		
+		
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		float browserVersion = 100.2f;
+		String browserName= "firefox";
+		boolean isVisible = true;
+		short releaseYear = 1998;
+		char browserLogo = 'f';
+		System.out.println(browserVersion +"\n" + browserName +"\n"+ isVisible +"\n"+ releaseYear +"\n" + browserLogo );
+
+		
+	}
+
+}
